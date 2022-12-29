69576e98db00d4ba9103733fc9d1ca51ebdb1e47
horse18leet <gluhih20003@mail.ru>
2022-12-29 22:41:26 +0300

    бебе

---
+++ Lab1/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿
+namespace лаба_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label17 = new System.Windows.Forms.Label();
+            this.numericUpDown9 = new System.Windows.Forms.NumericUpDown();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown8 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown7 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown6 = new System.Windows.Forms.NumericUpDown();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1293, 677);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Controls.Add(this.label17);
+            this.tabPage2.Controls.Add(this.numericUpDown9);
+            this.tabPage2.Controls.Add(this.dataGridView3);
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.numericUpDown8);
+            this.tabPage2.Controls.Add(this.numericUpDown7);
+            this.tabPage2.Controls.Add(this.numericUpDown6);
+            this.tabPage2.Controls.Add(this.label16);
+            this.tabPage2.Controls.Add(this.label15);
+            this.tabPage2.Controls.Add(this.label14);
+            this.tabPage2.Controls.Add(this.label13);
+            this.tabPage2.Controls.Add(this.label12);
+            this.tabPage2.Controls.Add(this.label11);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabPage2.Size = new System.Drawing.Size(1285, 648);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Матрица";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(66, 322);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(120, 28);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "Заполнить";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(81, 273);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(47, 17);
+            this.label17.TabIndex = 16;
+            this.label17.Text = "число";
+            // 
+            // numericUpDown9
+            // 
+            this.numericUpDown9.Location = new System.Drawing.Point(137, 271);
+            this.numericUpDown9.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.numericUpDown9.Name = "numericUpDown9";
+            this.numericUpDown9.Size = new System.Drawing.Size(49, 22);
+            this.numericUpDown9.TabIndex = 15;
+            this.numericUpDown9.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(272, 372);
+            this.dataGridView3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(936, 250);
+            this.dataGridView3.TabIndex = 14;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(272, 79);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(936, 242);
+            this.dataGridView2.TabIndex = 13;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(84, 389);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 28);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Сброс";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(84, 355);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 28);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Старт";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // numericUpDown8
+            // 
+            this.numericUpDown8.Location = new System.Drawing.Point(137, 204);
+            this.numericUpDown8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.numericUpDown8.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown8.Minimum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown8.Name = "numericUpDown8";
+            this.numericUpDown8.Size = new System.Drawing.Size(49, 22);
+            this.numericUpDown8.TabIndex = 10;
+            // 
+            // numericUpDown7
+            // 
+            this.numericUpDown7.Location = new System.Drawing.Point(137, 238);
+            this.numericUpDown7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.numericUpDown7.Name = "numericUpDown7";
+            this.numericUpDown7.Size = new System.Drawing.Size(49, 22);
+            this.numericUpDown7.TabIndex = 9;
+            this.numericUpDown7.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown6
+            // 
+            this.numericUpDown6.Location = new System.Drawing.Point(156, 79);
+            this.numericUpDown6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.numericUpDown6.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown6.Name = "numericUpDown6";
+            this.numericUpDown6.Size = new System.Drawing.Size(49, 22);
+            this.numericUpDown6.TabIndex = 8;
+            this.numericUpDown6.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label16.Location = new System.Drawing.Point(655, 340);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(163, 17);
+            this.label16.TabIndex = 5;
+            this.label16.Text = "матрица - результат";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(81, 204);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(33, 17);
+            this.label15.TabIndex = 4;
+            this.label15.Text = "мин";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(81, 238);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(39, 17);
+            this.label14.TabIndex = 3;
+            this.label14.Text = "макс";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label13.Location = new System.Drawing.Point(71, 162);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(128, 17);
+            this.label13.TabIndex = 2;
+            this.label13.Text = "диапазон чисел";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(45, 79);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(93, 17);
+            this.label12.TabIndex = 1;
+            this.label12.Text = "размерность";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(655, 49);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(145, 17);
+            this.label11.TabIndex = 0;
+            this.label11.Text = "исходная матрица";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 693);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 13, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(1320, 26);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(718, 20);
+            this.toolStripStatusLabel1.Text = "Нули удалить, положительные элементы расставить по убыванию, а отрицательные по в" +
+    "озрастанию";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1320, 719);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Массивы";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown numericUpDown8;
+        private System.Windows.Forms.NumericUpDown numericUpDown7;
+        private System.Windows.Forms.NumericUpDown numericUpDown6;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.NumericUpDown numericUpDown9;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Lab1/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace лаба_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e) 
+        {
+            int matrixSize, maxValueNumberMatrix, minValueNumberMatrix; //обьявляем 3 целочисленные переменные
+
+            Random r = new Random(); //создаем экземпляр класса Random
+
+            //считываем значения из numericUpDown с формы
+            maxValueNumberMatrix = Convert.ToInt32(numericUpDown7.Value);  //максимальное значение элементов в массиве
+            minValueNumberMatrix = Convert.ToInt32(numericUpDown8.Value);  //минимальное значение элементов в массиве
+            matrixSize = Convert.ToInt32(numericUpDown6.Value); //длина массива 
+
+            int[,] M = new int[matrixSize,matrixSize]; //инциализируем двумерный массив, квадратная матрица размерностью matrixSize
+
+            for (int i = 0; i < matrixSize; i++) //обход по строкам массива
+            {
+                for (int j = 0; j < matrixSize; j++) //обход по столбцам массива
+                {
+                    M[i, j] = r.Next(minValueNumberMatrix, maxValueNumberMatrix); //присваивание каждому элементу массива случайное значение
+                }
+            }
+
+            dataGridView2.RowCount = M.GetLength(0); //задаём элементу формы DataGridView количество строк
+            dataGridView2.ColumnCount = M.GetLength(1); //задаём элементу формы DataGridView количество столбцов
+
+            for (int i = 0; i < M.GetLength(0); i++) //обход по строкам
+            {
+                for (int j = 0; j < M.GetLength(1); j++) //обход по столбцам
+                {
+                    dataGridView2
+                        .Rows[i]
+                        .Cells[j]
+                        .Value = M[i, j]; //присваивание каждой ячейке DataGridView значение из массива
+                }
+            }
+
+            int[,] N; //обьявление двумерного массива
+            int maxNegativeCount1 = Convert.ToInt32(numericUpDown9.Value); //считываем с формы число по условию
+
+            int CountNegativeElementsInColumn(int[,] A, int column, int row = 0) //рекурсивный метод по нахождению отритательных чисел в каждой строчке массива
+            {
+                if (row >= A.GetLength(0)) //работа в пределе массива
+                    return 0;
+
+                if (A[row, column] < 0)  //если число отрицательное
+                    return CountNegativeElementsInColumn(A, column, row + 1) + 1; //вызываем этот же метод, но переходим к следующей строке и инкрементируем кол-во отрицательных чисел
+
+                else //если число положительное
+                    return CountNegativeElementsInColumn(A, column, row + 1);
+            }
+
+            void CopyColumn(int[,] source, int columnFrom, int[,] target, int columnTo, int row = 0) //метод по переносу столбцов
+            {
+                if (row >= source.GetLength(0))
+                    return;
+
+                target[row, columnTo] = source[row, columnFrom];
+                CopyColumn(source, columnFrom, target, columnTo, row + 1);
+            }
+
+            int[,] DeleteSomeColumn(int[,] A, int maxNegativeCount, int column = 0, int savedColumnsCount = 0) //метод по удалению некоторого столбца
+            {
+                if (column >= A.GetLength(1))
+                    return new int[A.GetLength(0), savedColumnsCount + 1];
+
+                if (CountNegativeElementsInColumn(A, column) > maxNegativeCount)
+                    return DeleteSomeColumn(A, maxNegativeCount, column + 1, savedColumnsCount);
+
+                int[,] newArray = DeleteSomeColumn(A, maxNegativeCount, column + 1, savedColumnsCount + 1);
+                CopyColumn(A, column, newArray, savedColumnsCount + 1);
+
+                return newArray;
+            }
+
+            int MaxElementInRow(int[,] A, int row, int column = 0) //метод по нахождению максимального элемента в строках
+            {
+                if (column == A.GetLength(1) - 1)
+                    return A[row, column];
+
+                int tempMax = MaxElementInRow(A, row, column + 1);
+
+                if (tempMax > A[row, column])
+                    return tempMax;
+
+                else return A[row, column];
+            }
+
+            void FindMaxInRows(int[,] A, int row = 0) //запись в первый столбец максимальных элементов
+            {
+                if (row >= A.GetLength(0))
+                    return;
+
+                A[row, 0] = MaxElementInRow(A, row);
+
+                FindMaxInRows(A, row + 1);
+            }
+
+            int[,] Solve(int[,] A, int maxNegativeCount) //метод решения
+            {
+                int[,] result = DeleteSomeColumn(A, maxNegativeCount);
+
+                FindMaxInRows(result);
+
+                return result;
+            }
+           
+            N = Solve(M, maxNegativeCount1);
+
+            dataGridView3.RowCount = N.GetLength(0);
+            dataGridView3.ColumnCount = N.GetLength(1);
+
+            for (int i = 0; i < N.GetLength(0); i++)
+            {
+                for (int j = 0; j < N.GetLength(1); j++)
+                {
+                    dataGridView3
+                        .Rows[i]
+                        .Cells[j]
+                        .Value = N[i, j]; //запись обработаного массива в другой DataGridView
+
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e) //кнопка сброса
+        {
+            dataGridView2.Rows.Clear();
+            dataGridView2.Columns.Clear();
+            dataGridView3.Rows.Clear();
+            dataGridView3.Columns.Clear();
+            toolStripStatusLabel1.Text = "";
+            button3.Enabled = true;
+            numericUpDown6.Value = 1;
+            numericUpDown8.Value = 0;
+            numericUpDown7.Value = 1;
+            numericUpDown9.Value = 1;
+        }
+
+        private void button5_Click(object sender, EventArgs e) //кнопка заполнить для тестирования
+        {
+            Random r = new Random();
+            numericUpDown6.Value = r.Next(5,10);
+            numericUpDown8.Value = r.Next(-5, -1);
+            numericUpDown7.Value = r.Next(4, 10);
+            numericUpDown9.Value = r.Next(1, 5);
+        }
+    }
+}
